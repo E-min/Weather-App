345c6728b870182dd5d90e4235ce1f6d66105691
Mehmet Emin <eminkocabuga@gmail.com>
2023-01-27 19:04:24 +0300

    finished

---
+++ js/app.js
@@ -0,0 +1,74 @@
+const form = document.getElementById("form");
+const container = document.querySelector('.container');
+const search = document.getElementById("search");
+const mainContainer = document.getElementById('main');
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const location = search.value;
+  location
+    ? fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=32c5514ebf687e3924f638bc1d91f51a&units=metric`
+      )
+        .then((response) => response.json())
+        .then((data) => currentWeather(data))
+    : console.log("Enter a location");
+});
+
+form.addEventListener("click", () => {
+  container.style.top = '5%'
+});
+
+const currentWeather = (data) => {
+  const {main,weather,name, ...rest} = data;
+  console.log(data);
+  createWeatherContainer(weather[0], main,name);
+};
+
+const createWeatherContainer = (weather, main, name) => {
+  const {description, icon, ...restW} = weather;
+  const {temp, ...restM} = main;
+//   <section class="container--weather">
+//   <div class="location">
+//     <i class="fa-solid fa-location-dot"></i>
+//     <span id="locationName">Osmaniye</span>
+//   </div>
+//   <img src="https://openweathermap.org/img/wn/04d@2x.png" alt="">
+//     <span id="degree">32<sup>&deg;</sup></span>
+//   <span id="status">Bulutlu</span>
+// </section>
+ const section = document.createElement('section');
+ section.classList.add('container--weather');
+
+ const locationDiv = document.createElement('div');
+ locationDiv.classList.add('location');
+ 
+ const locationDot = document.createElement('i');
+ locationDot.setAttribute('class', 'fa-solid fa-location-dot');
+
+ const locationName = document.createElement('span');
+ locationName.setAttribute('id', 'locationName');
+ locationName.innerText = name;
+
+ const image = document.createElement('img');
+ image.setAttribute('src', `https://openweathermap.org/img/wn/${icon}@2x.png`);
+
+ const temperature = document.createElement('span');
+ temperature.setAttribute('id', 'degree');
+ temperature.innerText = temp.toFixed(0);
+
+ const symbol = document.createElement('sup');
+ symbol.innerHTML = '°';
+
+ const status = document.createElement('span');
+ status.setAttribute('id', 'status');
+ status.innerText = description;
+
+ mainContainer.prepend(section);
+ section.appendChild(locationDiv);
+ locationDiv.appendChild(locationDot);
+ locationDiv.appendChild(locationName);
+ section.appendChild(image);
+ section.appendChild(temperature);
+ temperature.appendChild(symbol)
+ section.appendChild(status);
+}
